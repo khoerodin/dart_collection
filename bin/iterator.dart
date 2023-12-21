eedabe170bf8ba66bb10b251e73e1b6a253a20c4
Khoerodin <heykhoerodin@gmail.com>
2023-12-22 05:52:54 +0700

    Iterator

---
+++ bin/iterator.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  var list = ['Andi', 'Udin', 'Bagas'];
+
+  for (var name in list) {
+    print(name);
+  }
+
+  final iterator = list.iterator;
+
+  // iterator.moveNext();
+  // print(iterator.current);
+
+  // iterator.moveNext();
+  // print(iterator.current);
+
+  // iterator.moveNext();
+  // print(iterator.current);
+
+  while (iterator.moveNext()) {
+    print(iterator.current);
+  }
+}
