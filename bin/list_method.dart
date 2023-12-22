b8bcc44795e6e3df6bf8f68e415d3bcb0deeafd6
Khoerodin <heykhoerodin@gmail.com>
2023-12-22 20:38:24 +0700

    List Method

---
+++ bin/list_method.dart
@@ -0,0 +1,6 @@
+void main() {
+  final names = ['Andi', 'Eko', 'Budi', 'Bagus', 'Slamet'];
+  names.insert(1, 'Kurniawan');
+
+  print(names);
+}
---
+++ bin/list_operator.dart
@@ -0,0 +1,7 @@
+void main() {
+  final names1 = ['Budi', 'Andi'];
+  final names2 = ['Anita', 'Sofia'];
+  final names3 = names1 + names2;
+
+  print(names3);
+}
