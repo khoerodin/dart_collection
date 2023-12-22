8cd20374aa2610ec943fc363a6e1632849740140
Khoerodin <heykhoerodin@gmail.com>
2023-12-22 19:18:52 +0700

    Unmodifiable Set

---
+++ bin/unmodifiable_set.dart
@@ -0,0 +1,8 @@
+import 'dart:collection';
+
+void main() {
+  final dataku = <int>{1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+  final unmodifiableDataku = UnmodifiableSetView(dataku);
+
+  // unmodifiableDataku.add(10); // ini akan error
+}
