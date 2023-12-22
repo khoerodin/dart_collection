8dcc40a5589e70ccded814f5b6c1003d544999c1
Khoerodin <heykhoerodin@gmail.com>
2023-12-22 20:30:37 +0700

    Iterable Properties

---
+++ bin/iterable_properties.dart
@@ -0,0 +1,9 @@
+void main() {
+  final angka2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
+  print(angka2.length);
+  print(angka2.first);
+  print(angka2.last);
+  print(angka2.isEmpty);
+  // print(angka2.single); // error
+}
